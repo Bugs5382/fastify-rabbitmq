6faf789eb580a0f400cbaa9f59ea2c94d43f6564
Bugs5382 <bugs5382@users.noreply.github.com>
2023-08-10 17:45:35 -0400

    feature: initial code #7
    this will now wrap "amqp-connection-manager" around fastify instead of writing my own implementation.

--- index.ts
+++ index.ts
@@ -1,86 +1,36 @@
-import { randomUUID } from 'crypto'
+import amqp from "amqp-connection-manager";
+import type {
+  AmqpConnectionManagerOptions,
+  ConnectionUrl,
+} from "amqp-connection-manager"
+import {IAmqpConnectionManager} from "amqp-connection-manager/dist/types/AmqpConnectionManager";
 import { FastifyInstance, FastifyPluginCallback } from 'fastify'
 import fp from 'fastify-plugin'
-import amqp, { Channel, ConsumeMessage } from 'amqplib'
 
-import FastifyRabbitMQOptions = fastifyRabbitMQ.FastifyRabbitMQOptions
+import FastifyRabbitMQOptions = fastifyRabbitMQ.FastifyRabbitMQOptions;
 
 declare module 'fastify' {
+
   interface FastifyInstance {
-    rabbitmq: fastifyRabbitMQ.FastifyRabbitMQObject & fastifyRabbitMQ.FastifyRabbitMQNestedObject
+    rabbitmq: IAmqpConnectionManager & fastifyRabbitMQ.FastifyRabbitMQNestedObject
   }
 
 }
 
 type fastifyRabbitMQPlugin = FastifyPluginCallback<
-NonNullable<fastifyRabbitMQ.FastifyRabbitMQOptions>
+  NonNullable<fastifyRabbitMQ.FastifyRabbitMQOptions>
 >
 
 declare namespace fastifyRabbitMQ {
 
-  export interface FastifyRabbitMQObject {
-    /**
-     * createBind
-     * @param queue
-     * @param consumeMessageFn
-     */
-    createBind: (queue: string, consumeMessageFn: (message: ConsumeMessage | null, channel: Channel) => void) => Promise<void>
-    /**
-     * createBindRPC
-     * @param queue
-     * @param consumeMessageFn
-     */
-    createBindRPC: (queue: string, consumeMessageFn: (message: ConsumeMessage | null, channel: Channel) => void) => Promise<string>
-    /**
-     * createExchange
-     * @param exchangeName
-     * @param exchangeType
-     * @param consumeMessageFn
-     */
-    createExchange: (exchangeName: string, exchangeType: string, consumeMessageFn: (message: ConsumeMessage | null, channel: Channel) => void) => Promise<void>
-    /**
-     * directMessage
-     * @param sendQueue
-     * @param toQueue
-     * @param message
-     * @param preProcessMessage
-     */
-    directMessage: (sendQueue: string, toQueue: string, message: any, preProcessMessage: (message: any) => string) => void
-    /**
-     * publishMessage
-     * @param sendQueue
-     * @param message
-     * @param preProcessMessage
-     */
-    publishMessage: (sendQueue: string, message: any, preProcessMessage: (message: any) => string) => void
-    /**
-     * publishMessageExchange
-     * @param queue
-     * @param routingKey
-     * @param message
-     * @param preProcessMessage
-     */
-    publishMessageExchange: (queue: string, routingKey: string, message: any, preProcessMessage: (message: any) => string) => void
-    /**
-     * publishRPC
-     * @param rpcQueue
-     * @param message
-     * @param preProcessMessage
-     * @param timeout
-     */
-    publishRPC: (rpcQueue: string, message: any, preProcessMessage: (message: any) => string, timeout?: number) => Promise<string>
-  }
-
   export interface FastifyRabbitMQNestedObject {
-    [namespace: string]: FastifyRabbitMQObject
+    [namespace: string]: IAmqpConnectionManager
   }
 
-  export interface FastifyRabbitMQOptions {
-    host?: string
+  export interface FastifyRabbitMQOptions extends AmqpConnectionManagerOptions {
+    logLevel?: 'trace' | 'debug' | 'info' | 'warn' | 'error'
     namespace?: string
-    password?: string
-    port?: string
-    username?: string
+    urLs: ConnectionUrl | ConnectionUrl[] | undefined | null
   }
 
   export const fastifyRabbitMQPlugin: fastifyRabbitMQPlugin
@@ -92,192 +42,46 @@ declare namespace fastifyRabbitMQ {
  * decorateFastifyInstance
  * @since 0.0.1
  * @param fastify
- * @param options
- * @param functions
+ * @param connection
  */
-const decorateFastifyInstance = (fastify: FastifyInstance, options: FastifyRabbitMQOptions, functions: any): void => {
-  const { namespace } = options
-
-  if (namespace) {
-    if (typeof fastify.rabbitmq === 'undefined') {
-      fastify.decorate('rabbitmq', { ...functions })
-    }
-    if (typeof fastify.rabbitmq[namespace] !== 'undefined') {
-      throw Error('Connection name already registered: ' + namespace)
-    }
-  } else {
-    if (typeof fastify.rabbitmq !== 'undefined') {
-      throw Error('fastify-rabbitmq has already registered')
-    }
-  }
-
-  if (typeof fastify.rabbitmq === 'undefined') {
-    fastify.decorate('rabbitmq', { ...functions })
-  }
+const decorateFastifyInstance = (fastify: FastifyInstance, connection: any): void => {
+  fastify.log.trace('[fastify-rabbitmq] Decorate Fastify')
+  fastify.decorate('rabbitmq', connection )
 }
 
 const fastifyRabbit = fp(async (fastify: FastifyInstance, options: FastifyRabbitMQOptions): Promise<void> => {
+
   const {
-    host = 'localhost',
-    port = '5672',
-    username = 'guest',
-    password = 'guest'
+    logLevel = 'silent',
+    urLs,
+    heartbeatIntervalInSeconds,
+    reconnectTimeInSeconds,
+    findServers,
+    connectionOptions
   } = options
 
-  const RABBITMQ_FULL_URL = `amqp://${username}:${password}@${host}:${port}`
+  // override log level
+  fastify.log.child({}, {level: logLevel})
 
-  /**
-   * getRabbitChannel
-   * @since 0.0.1
-   */
-  async function getRabbitChannel (): Promise<Channel> {
-    const conn = await amqp.connect(RABBITMQ_FULL_URL)
-    return await conn.createChannel()
-  }
+  const connection = amqp.connect(urLs, {
+    heartbeatIntervalInSeconds,
+    reconnectTimeInSeconds,
+    findServers,
+    connectionOptions
+  })
 
-  // create the channel
-  const channel = await getRabbitChannel()
+  connection.on('connect', function() {
+    fastify.log.debug('[fastify-rabbitmq] Connection to RabbitMQ Successful');
+  });
 
-  /**
-   * createBind
-   * @since 0.0.1
-   * @param queue
-   * @param consumeMessageFn
-   */
-  async function createBind (queue: string, consumeMessageFn: (message: ConsumeMessage | null, channel: Channel) => void): Promise<void> {
-    await channel?.assertQueue(queue, { durable: false }).then(async () => {
-      fastify.log.debug('[X] Waiting Queue (%s)', queue)
-      await channel.consume(queue, async (msg: amqp.ConsumeMessage | null): Promise<void> => {
-        consumeMessageFn(msg, channel)
-      }, {
-        noAck: true
-      })
-    })
-  }
-
-  /**
-   * createBindRPC
-   * Create an RPC queue for returning data back to the client from the server.
-   * @since 0.0.1
-   * @param queue {string}
-   * @param consumeMessageFn {function}
-   */
-  async function createBindRPC (queue: string, consumeMessageFn: (message: ConsumeMessage | null, channel: Channel) => Promise<string>): Promise<void> {
-    await channel?.assertQueue(queue + '-rpc', { durable: false }).then(async () => {
-      await channel.prefetch(1)
-      fastify.log.debug('[X] Waiting RPC Server Queue (%s)', queue + '-rpc')
-      await channel.consume(queue + '-rpc', async (msg: amqp.ConsumeMessage | null): Promise<void> => {
-        fastify.log.debug('[X] RPC Server Correlation ID: %s', msg?.properties.correlationId)
-        fastify.log.debug('[X] RPC Server Data Received: %s', JSON.parse(JSON.stringify(msg?.content.toString())))
-        const response = await consumeMessageFn(msg, channel)
-        channel.sendToQueue(msg?.properties.replyTo, Buffer.from(response), {
-          correlationId: msg?.properties.correlationId
-        })
-      }, {
-        noAck: true
-      })
-    })
-  }
-
-  /**
-   * createExchange
-   * @since 0.0.1
-   * @param exchangeName
-   * @param exchangeType
-   * @param consumeMessageFn
-   */
-  async function createExchange (exchangeName: string, exchangeType: string, consumeMessageFn: (message: ConsumeMessage | null, channel: Channel) => void): Promise<void> {
-    await channel?.assertExchange(exchangeName + '-exchange', exchangeType, { durable: true }).then(async () => {
-      await channel.consume(exchangeName + '-exchange', async (msg: amqp.ConsumeMessage | null): Promise<void> => {
-        consumeMessageFn(msg, channel)
-      }, {
-        noAck: true
-      })
-    })
-  }
-
-  /**
-   * directMessage
-   * @since 0.0.1
-   * @param sendQueue {string} The target queue and directed to that queue. No one else can take it.
-   * @param toQueue {string} Make sure any response that is returned goes to this queue. There must be a string that is returned. It doesn't have to be returned from the same queue that it originally processed on.
-   * @param message {any} You can publish anything as long as it goes out over as a string.
-   * @param preProcessMessage {function} A function that process the message prior to being sent out.
-   */
-  function directMessage (sendQueue: string, toQueue: string = '', message: any, preProcessMessage: (message: any) => string): void {
-    const msg = preProcessMessage(message)
-    channel?.sendToQueue(sendQueue, Buffer.from(msg), {
-      replyTo: toQueue
-    })
-  }
-
-  /**
-   * publishMessage
-   * @since 0.0.1
-   * @param sendQueue {string} The target queue.
-   * @param message {any} You can publish anything as long as it goes out over as a string.
-   * @param preProcessMessage {function} A function that process the message prior to being sent out.
-   */
-  function publishMessage (sendQueue: string, message: any, preProcessMessage: (message: any) => string): void {
-    const msg = preProcessMessage(message)
-    fastify.log.debug('[X] Publish to Queue (%s): %s', '', [sendQueue, msg])
-    channel?.publish('', sendQueue, Buffer.from(msg))
-  }
-
-  /**
-   * publishMessageExchange
-   * @since 0.0.1
-   * @param queue
-   * @param routingKey {string} The target queue.
-   * @param message {any} You can publish anything as long as it goes out over as a string.
-   * @param preProcessMessage {function} A function that process the message prior to being sent out.
-   */
-  function publishMessageExchange (queue: string, routingKey: string, message: any, preProcessMessage: (message: any) => string): void {
-    const msg = preProcessMessage(message)
-    fastify.log.debug('[X] Publish to Queue (%s), Routing Queue (%s): %s', queue, routingKey, msg)
-    channel?.publish(queue + '-exchange', routingKey, Buffer.from(msg))
-  }
-
-  /**
-   * publishRPC
-   * Send an RPC message to an RPC Queue
-   * @since 0.0.1
-   * @param rpcQueue {string} The RPC queue.
-   * You do not need to add the "-rpc" to the name.
-   * This is done automatically.
-   * @param message {any} You can publish anything as long as it goes out over as a string.
-   * @param preProcessMessage {function} A function that process the message prior to being sent out.
-   * @param timeout {number} Time in milliseconds to close out the client before not accepting a result.
-   * By default, it is 100ms.
-   */
-  async function publishRPC (rpcQueue: string, message: any, preProcessMessage: (message: any) => string, timeout: number = 100) {
-    channel?.assertQueue('', { exclusive: true }).then(async (q) => {
-      const correlationId = randomUUID()
-      const msgProcess = preProcessMessage(message)
-      fastify.log.debug('[X] RPC Client Correlation ID: ' + correlationId)
-      fastify.log.debug('[X] RPC Client Message: ' + msgProcess)
-      await channel.consume(q.queue, async (msg: amqp.ConsumeMessage | null): Promise<any> => {
-        if (msg?.properties.correlationId === correlationId) {
-          fastify.log.debug('[X] RPC Client Got Message: ' + msg.content.toString())
-          setTimeout(async () => {
-            await channel.close()
-            process.exit(0)
-          }, timeout)
-        }
-      }, {
-        noAck: true
-      })
-      channel?.sendToQueue(rpcQueue + '-rpc', Buffer.from(msgProcess), {
-        correlationId,
-        replyTo: q.queue
-      })
-    })
-  }
+  connection.on('disconnect', function() {
+    fastify.log.debug('[fastify-rabbitmq] Connection to RabbitMQ Disconnected');
+  });
 
   /**
    * Decorate Fastify
    */
-  decorateFastifyInstance(fastify, options, { createExchange, createBind, createBindRPC, directMessage, publishMessage, publishMessageExchange, publishRPC })
+  decorateFastifyInstance(fastify, {...connection})
 })
 
 export default fastifyRabbit
