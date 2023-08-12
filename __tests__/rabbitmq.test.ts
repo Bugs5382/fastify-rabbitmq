21ae251a9cf530408fc29fb71fb7f69512db3178
Bugs5382 <bugs5382@users.noreply.github.com>
2023-08-12 07:56:30 -0400

    feature: basic unit test #9
    added in workflow // basic "register" test added for now

--- __tests__/rabbitmq.test.ts
+++ __tests__/rabbitmq.test.ts
@@ -1,5 +1,22 @@
+import fastify, {FastifyInstance} from "fastify";
+import fastifyRabbit from "../src";
+
+let app: FastifyInstance;
+
+beforeEach(() => {
+  app = fastify();
+});
+
+afterEach(async () => {
+  await app.close();
+});
+
 describe('fastify-rabbitmq', () => {
-  it('simple test', async () => {
-    expect({}).toStrictEqual({});
+
+  it("check to make sure we don't error out", async () => {
+    app.register(fastifyRabbit).ready((err) => {
+      expect(err).toBeUndefined();
+    })
   });
+
 });
