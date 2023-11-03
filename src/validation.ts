7edd6f7a69079165b25cd84c244323712bfe0ad3
Shane <Bugs5382@users.noreply.github.com>
2023-11-03 12:34:13 -0400

    15 jest fix open handlers (#16)
    * feature: updated unit test
    
    #15 fixed Jest Open Handlers for Fastify // Updated All Packages // Cleanup main "plugin" to remove async issues
    
    * feature: more unit tests
    
    #15 a while bunch of new unit tests
    
    * test: fixed unit tests
    
    #15 namespace and regular "twice" was causing an issue // now all unit tests fixed.
    
    * test: fixed unit tests x2
    
    #15
    
    * test: unit test now pass
    
    #15 can't get JEST to exit correctly... ugh

---
+++ src/validation.ts
@@ -0,0 +1,23 @@
+import { errors } from './errors'
+
+export const validateOpts = async (opts: any): Promise<void> => {
+  // Mandatory
+  if (typeof opts.urLs === 'undefined' && typeof opts.findServers === 'undefined') {
+    throw new errors.FASTIFY_RABBIT_MQ_ERR_INVALID_OPTS('urLs or findServers must be defined.')
+  }
+
+  // Mandatory
+  if (typeof opts.urLs !== 'undefined' && opts.urLs.length < 1) {
+    throw new errors.FASTIFY_RABBIT_MQ_ERR_INVALID_OPTS('urLs must have one item in the array.')
+  }
+
+  // Optional
+  if (typeof opts.heartbeatIntervalInSeconds !== 'undefined' && opts.heartbeatIntervalInSeconds < 0) {
+    throw new errors.FASTIFY_RABBIT_MQ_ERR_INVALID_OPTS('heartbeatIntervalInSeconds must be a valid number greater than 0.')
+  }
+
+  // Optional
+  if (typeof opts.reconnectTimeInSeconds !== 'undefined' && opts.reconnectTimeInSeconds < 0) {
+    throw new errors.FASTIFY_RABBIT_MQ_ERR_INVALID_OPTS('reconnectTimeInSeconds must be a valid number greater than 0.')
+  }
+}
