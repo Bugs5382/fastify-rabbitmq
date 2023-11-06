d4a256cb0b3e445c1cd88af9b445ea99ac681514
Bugs5382 <bugs5382@users.noreply.github.com>
2023-11-06 07:18:16 -0500

    feature: first draft
    #19 first draft of a building RPC client into a system // created working findChannel function [ci-skip]

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,39 @@
-import { Channel, ConfirmChannel, ConsumeMessage } from 'amqplib'
-import amqp from 'amqp-connection-manager'
-import { FastifyInstance } from 'fastify'
+import {Channel, ConfirmChannel, ConsumeMessage, Replies} from 'amqplib'
+import amqp, { AmqpConnectionManager, ChannelWrapper, ConnectionUrl } from 'amqp-connection-manager'
+import {randomUUID} from "crypto";
+import { FastifyInstance, FastifyPluginOptions } from 'fastify'
 import fp from 'fastify-plugin'
 import { errors } from './errors'
 import { validateOpts } from './validation'
-import { AmqpConnectionManager, ConnectionUrl } from 'amqp-connection-manager'
-import { FastifyPluginOptions } from 'fastify'
+
+import FastifyRabbitMQOptions = fastifyRabbitMQ.FastifyRabbitMQOptions
+
+
+/**
+ * Custom Override which builds in extendability of ChannelWrapper
+ */
+export  interface FastifyRabbitMQChannelWrapper extends  ChannelWrapper {
+  /* Number of seconds to wait before */
+  ttl: number;
+  /* Send RPC Message */
+  sendRPC: (msg: any, ttl: number, exchangeName: string, routingKey: string) => void;
+  /* Correlation ID */
+  corr: string;
+}
+
+/**
+ * Custom Override which builds in extendability of AmqpConnectionManager
+ */
+export interface FastifyRabbitMQAmqpConnectionManager extends AmqpConnectionManager {
+  /* List of Channels */
+  _channels: ChannelWrapper[]
+  /* Create an RPC Client */
+  createRPCClient: (queue_name: string, ttl: number, setup: () => any) => Promise<ChannelWrapper>
+  /* Create an RPC Server */
+  createRPCServer: () => Promise<ChannelWrapper>
+  /* Find Channels */
+  findChannel: (name: string) => ChannelWrapper | undefined
+}
 
 declare module 'fastify' {
 
@@ -13,7 +41,7 @@ declare module 'fastify' {
     /**
      * RabbitMQ Connection
      */
-    rabbitmq: AmqpConnectionManager & fastifyRabbitMQ.FastifyRabbitMQAmqpNestedObject
+    rabbitmq: FastifyRabbitMQAmqpConnectionManager & fastifyRabbitMQ.FastifyRabbitMQAmqpNestedObject
   }
 
 }
@@ -24,7 +52,7 @@ declare namespace fastifyRabbitMQ {
     /**
      * Nested Namespace
      */
-    [namespace: string]: AmqpConnectionManager
+    [namespace: string]: FastifyRabbitMQAmqpConnectionManager
   }
 
   export interface FastifyRabbitMQOptions extends FastifyPluginOptions {
@@ -43,8 +71,6 @@ declare namespace fastifyRabbitMQ {
   }
 
 }
-
-import FastifyRabbitMQOptions = fastifyRabbitMQ.FastifyRabbitMQOptions
 
 /**
  * decorateFastifyInstance
@@ -107,12 +133,13 @@ const fastifyRabbit = fp<FastifyRabbitMQOptions>(async (fastify, options, done) 
   // override log level
   const logger = fastify.log.child({}, { level: logLevel })
 
-  const connection = amqp.connect(urLs, {
+  // we need this 'writeable'
+  let connection = amqp.connect(urLs, {
     heartbeatIntervalInSeconds,
     reconnectTimeInSeconds,
     connectionOptions,
     findServers
-  })
+  }) as FastifyRabbitMQAmqpConnectionManager
 
   connection.on('connect', function () {
     logger.debug('[fastify-rabbitmq] Connection to RabbitMQ Successful')
@@ -121,6 +148,90 @@ const fastifyRabbit = fp<FastifyRabbitMQOptions>(async (fastify, options, done) 
   connection.on('connectFailed', function () {
     logger.debug('[fastify-rabbitmq] Connection to RabbitMQ Connection Failed')
   })
+
+  connection.createRPCServer = async function () {
+    const channelWrapper = connection.createChannel({
+      json: true,
+      setup: () => {}
+    })
+    return channelWrapper
+  }
+
+  connection.createRPCClient = async function (queue_name: string = "", ttl: number = 0, setup: Function) {
+    let channelWrapper = connection.createChannel({
+      json: true,
+      setup: function (channel: Channel) {
+        channelWrapper.ttl = ttl;
+        return new Promise(async function (resolve, reject) {
+          try {
+
+            let queue: Promise<Replies.AssertQueue> | any;
+            if (typeof setup !== "function") {
+              queue = await channel.assertQueue('', { exclusive: true });
+            } else {
+              queue = await setup(channel);
+            }
+
+            channelWrapper.sendRPC = async function (msg, ttl, exchangeName = "", routingKey = queue_name) {
+              let corr = randomUUID();
+              channelWrapper.corr = corr;
+              await channelWrapper.publish(exchangeName, routingKey, msg, {
+                correlationId: corr,
+                replyTo: queue.queue,
+                expiration: (ttl !== undefined ? ttl : channelWrapper.ttl) * 1000
+              });
+              return await getResponse(channelWrapper, corr, channelWrapper.ttl);
+            };
+
+            await channel.consume(queue.queue, function (msg) {
+              let cache = channelWrapper.cache;
+              if (cache) {
+                let value = cache.get(msg?.properties.correlationId);
+                if (value) {
+                  cache.del(msg?.properties.correlationId);
+                  try {
+                      let json = JSON.parse(msg.content.toString());
+                      if (json.err) {
+                        value.reject(json.err);
+                      } else {
+                        value.resolve(json.msg);
+                      }
+                  } catch (err) {
+                    value.reject(err);
+                  }
+                }
+              }
+            }, {
+              noAck: true
+            });
+
+            resolve();
+
+          } catch (err) {
+            reject(err);
+          }
+        });
+      }
+    }) as FastifyRabbitMQChannelWrapper;
+
+    if (channelWrapper.sendRPC === null) {
+      channelWrapper.sendRPC = async () => {
+        throw new Error('Channel Not Ready');
+      };
+    }
+
+    return channelWrapper
+  }
+
+  /**
+   * Find Channel
+   * @since 1.0.0
+   * @param name {string} Name of the Queue
+   * @returns ChannelWrapper | undefined
+   */
+  connection.findChannel = function (name: string): ChannelWrapper | undefined {
+    return connection._channels.find(channel => channel.name == name) || undefined
+  }
 
   /**
    * Decorate Fastify
