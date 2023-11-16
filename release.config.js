7d81893ad6f1bd758b2ab9ecdeaa81791d7b4731
Bugs5382 <bugs5382@users.noreply.github.com>
2023-11-16 11:19:57 -0500

    ci: release config finished
    #19

--- release.config.js
+++ release.config.js
@@ -1,10 +1,18 @@
 module.exports = {
-  verifyConditions: [
+  "branches": ["main"],
+  "verifyConditions": [
     '@semantic-release/changelog',
     '@semantic-release/npm',
     '@semantic-release/git'
   ],
-  prepare: [
+  "plugins": [
+    '@semantic-release/commit-analyzer',
+    '@semantic-release/release-notes-generator',
+    '@semantic-release/changelog',
+    '@semantic-release/npm',
+    '@semantic-release/git'
+  ],
+  "prepare": [
     '@semantic-release/changelog',
     '@semantic-release/npm',
     '@semantic-release/git'
