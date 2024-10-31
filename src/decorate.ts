7a75004b5108e63afbe6e2a69e22fbb3c3949ea5
Bugs5382 <bugs5382@users.noreply.github.com>
2024-10-31 13:20:02 -0400

    ci: update workflows
    - add in a version for typedoc

--- src/decorate.ts
+++ src/decorate.ts
@@ -3,12 +3,12 @@ import { ConnectionOptions } from 'rabbitmq-client'
 export interface FastifyRabbitMQOptions {
   /**
    * @since 1.0.0
-   * @description Connection String or object pointing to the RabbitMQ Broker Services
+   * @remarks Connection String or object pointing to the RabbitMQ Broker Services
    */
   connection: string | ConnectionOptions
   /**
    * @since 1.0.0
-   * @description To set the custom nNamespace within this plugin instance. Used to register this plugin more than one time.
+   * @remarks To set the custom nNamespace within this plugin instance. Used to register this plugin more than one time.
    */
   namespace?: string
 }
